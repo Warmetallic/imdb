70ea59ec197f0f90a4b14de5d863aa5975a53a27
Gleb Kharlamov <scream10@list.ru>
2023-04-11 05:11:47 +0300

    tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -4,5 +4,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <h1 class="text-4xl text-red-600 font-gothic text-center border-b-2 border-red-600">
+      Your heading text here
+    </h1>
+  );
 }
