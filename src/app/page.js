5572f44d2bb9c6f81f8acfa92313c9d66bb26d24
Gleb Kharlamov <scream10@list.ru>
2023-04-11 23:01:52 +0300

    header

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <h1 class="text-4xl text-red-600 font-gothic text-center border-b-2 border-red-600">
-      Your heading text here
-    </h1>
-  );
+  return <h1>Home</h1>;
 }
