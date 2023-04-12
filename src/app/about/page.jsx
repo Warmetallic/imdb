da9bdb612e5537c214bf169483ef37f1fda8c70d
Gleb Kharlamov <scream10@list.ru>
2023-04-12 21:11:51 +0300

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nemo
+        obcaecati? Perferendis veniam atque odio repudiandae quia eius eligendi
+        esse magnam numquam consequatur totam minima veritatis ea facere
+        blanditiis molestias, hic ducimus rem dolor illum earum similique
+        reprehenderit? Dicta ea sint necessitatibus dolore nisi rem architecto
+        ullam explicabo numquam!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nemo
+        obcaecati? Perferendis veniam atque odio repudiandae quia eius eligendi
+        esse magnam numquam consequatur totam minima veritatis ea facere
+        blanditiis molestias, hic ducimus rem dolor illum earum similique
+        reprehenderit? Dicta ea sint necessitatibus dolore nisi rem architecto
+        ullam explicabo numquam!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nemo
+        obcaecati? Perferendis veniam atque odio repudiandae quia eius eligendi
+        esse magnam numquam consequatur totam minima veritatis ea facere
+        blanditiis molestias, hic ducimus rem dolor illum earum similique
+        reprehenderit? Dicta ea sint necessitatibus dolore nisi rem architecto
+        ullam explicabo numquam!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nemo
+        obcaecati? Perferendis veniam atque odio repudiandae quia eius eligendi
+        esse magnam numquam consequatur totam minima veritatis ea facere
+        blanditiis molestias, hic ducimus rem dolor illum earum similique
+        reprehenderit? Dicta ea sint necessitatibus dolore nisi rem architecto
+        ullam explicabo numquam!
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/app/testPage/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function testPage() {
+  return <div>testPage</div>;
+}
+
+export default testPage;
